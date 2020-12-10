325fca1ee69cce4476f95ea6e1cb8c0781cb57bd
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-12-10 22:17:54 +0100

    Tuto News Section

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
+$routes->setAutoRoute(false);
 
 /**
  * --------------------------------------------------------------------
@@ -31,6 +31,10 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('(:any)', 'Pages::view/$1');
+
+$routes->get('news/(:segment)', 'News::view/$1');
+$routes->get('news', 'News::index');
 $routes->get('(:any)', 'Pages::view/$1');
 
 /**
